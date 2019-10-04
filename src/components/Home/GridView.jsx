1954e5a746957021bbf744d67b826d16ac8fd253
mayank1112@@gmail.com <mayank1112@@gmail.com>
2019-10-04 01:02:16 +0100

    Grid view

---
+++ src/components/Home/GridView.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import TvShowItem from "./TvShowItem";
+
+const GridView = ({ tvShows }) => (
+    tvShows.length > 0
+    && tvShows.map(
+        tvShow => <TvShowItem key={tvShow.id} tvShow={tvShow} />
+    )
+);
+
+GridView.propTypes = {
+    tvShows: PropTypes.arrayOf({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        poster_path: PropTypes.string.isRequired,
+        vote_average: PropTypes.number.isRequired,
+    }).isRequired,
+};
+
+export default GridView;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../css/sainsburysTheme.css";
 import urls from "../../constants/urls"
-import TvShowItem from './TvShowItem';
+import GridView from './GridView';
 import intl from '../../locale/enUk.js';
 
 class Home extends React.Component {
@@ -12,7 +12,7 @@ class Home extends React.Component {
   componentDidMount() {
     fetch(urls.shows)
         .then(response => response.json())
-        .then(tvShows => this.setState({tvShows: tvShows.results}));
+        .then(tvShows => this.setState({ tvShows: tvShows.results }));
   }
   render() {
     const { tvShows } = this.state;
@@ -22,12 +22,7 @@ class Home extends React.Component {
           <h2>{bannerTitle}</h2>
           <h2>{bannerSubTitle}</h2>
           <h3>{homePageTitle}</h3>
-          {
-            tvShows.length > 0
-                && tvShows.map(
-                    tvShow => <TvShowItem key={tvShow.id} tvShow={tvShow} />
-                )
-          }
+          <GridView tvShows={tvShows} />
         </React.Fragment>
     );
   }
